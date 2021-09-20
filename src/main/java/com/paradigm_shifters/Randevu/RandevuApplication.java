f21115b1b9824ebddbf82d50b7da74d2363b474b
anghielo <user_noreply@github.com>
2021-09-20 01:39:00 -0700

    Added first Hello World html page with Get command

--- src/main/java/com/paradigm_shifters/Randevu/RandevuApplication.java
+++ src/main/java/com/paradigm_shifters/Randevu/RandevuApplication.java
@@ -2,12 +2,19 @@ package com.paradigm_shifters.Randevu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class RandevuApplication {
+public class RandevuApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandevuApplication.class, args);
 	}
 
+	// Pass a simple path for the view controller to handle GET requests. Requires the lombok dependency
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("hello_world01");
+	}
 }
