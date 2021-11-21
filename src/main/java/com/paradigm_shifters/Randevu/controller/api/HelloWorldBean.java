766dd3176309ea564d049ceee2cf32e9be7c5819
anghielo <user_noreply@github.com>
2021-11-20 19:58:03 -0800

    Update

---
+++ src/main/java/com/paradigm_shifters/Randevu/controller/api/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.paradigm_shifters.Randevu.controller.api;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
