ff245c5c7970ef8bad391267824b939edd6a695a
anghielo <user_noreply@github.com>
2021-11-21 06:32:06 -0800

    Added classes and updated test cases

--- src/main/java/com/paradigm_shifters/Randevu/controller/api/UserResource.java
+++ src/main/java/com/paradigm_shifters/Randevu/controller/api/UserResource.java
@@ -3,8 +3,11 @@ package com.paradigm_shifters.Randevu.controller.api;
 import com.paradigm_shifters.Randevu.dao.UserDaoService;
 import com.paradigm_shifters.Randevu.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,12 +23,24 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
 
+        // CREATED
+        // /users/{id}       savedUser.Id()
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        // Returns status code "201"
+        return ResponseEntity.created(location).build();
     }
 }
