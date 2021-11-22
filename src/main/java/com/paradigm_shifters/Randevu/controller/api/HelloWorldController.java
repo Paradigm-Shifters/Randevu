38a4b292a842d3c54d69ff43e1bd67613c923b88
anghielo <user_noreply@github.com>
2021-11-21 17:45:24 -0800

    Added classes and updated test cases

--- src/main/java/com/paradigm_shifters/Randevu/controller/api/HelloWorldController.java
+++ src/main/java/com/paradigm_shifters/Randevu/controller/api/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.paradigm_shifters.Randevu.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     //GET
     //URI - /hello-world
@@ -26,5 +34,17 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+
+        return messageSource.getMessage(
+                "good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
+        //return "Hello World";
+
+        // en = Hello World
+        // nl = Goede Morgen
+        // fr = Bonjour
+    }
 
 }
